43b807f957a999aff42fbc8924d9a94fce22a3ee
Gad Azeraf <gad.azeraf@epita.fr>
2018-10-15 13:15:34 +0200

    Deploy RES contracts with BTU Token ropsten address

--- RES/migrations/2_deploy_contracts.js
+++ RES/migrations/2_deploy_contracts.js
@@ -1,25 +1,29 @@
 const RES = artifacts.require('./RES.sol')
-const btuTokenSaleABI = require('../../BTUToken/build/contracts/BTUTokenSale')
+console.log('check');
+import BTUabstraction from 'BTUTOKEN/BTU.json'
+import BTUTokenSaleabstraction from 'BTUTOKEN/BTUTokenSale.json'
 const Web3 = require('web3')
 var fs = require('fs')
 web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'))
 
 module.exports = function (deployer, networks, accounts) {
 
-    console.log("TokenSale address = " + btuTokenSaleABI.networks[deployer.network_id].address)
-    let btuTokenSale = new web3.eth.Contract(btuTokenSaleABI.abi, btuTokenSaleABI.networks[deployer.network_id].address)
-    btuTokenSale.methods.btuToken().call(function (err, btuAddress) {
+    const BTUTokenSale = await getContract(web3, BTUTokenSaleabstraction)
+    const BTU = await getContract(web3, BTUabstraction)
+    //console.log("TokenSale address = " + btuTokenSaleABI.networks[deployer.network_id].address)
+    // let btuTokenSale = new web3.eth.Contract(btuTokenSaleABI.abi, "0x1ab498774cdfb7eb4df1c8c2634ea7d875b5374a" )
+    /*btuTokenSale.methods.btuToken().call(function (err, btuAddress) {
         if (err) {
             console.log("Error: " + err)
             return
-        }
-        console.log("BTU address = " + btuAddress)
-        return deployer.deploy(RES, btuAddress).then(() => {
-            RESabi = require('../build/contracts/RES.json')
+        }*/
+        console.log("BTU address = " + BTU.address)
+        return deployer.deploy(RES, BTU.address).then(() => {
+            RESabi = require('build/contracts/RES.json')
             RESabi.networks[deployer.network_id] = {}
             RESabi.networks[deployer.network_id].address = RES.address
-            fs.writeFileSync('../RES/build/contracts/RES.json', JSON.stringify(RESabi, null, 4))
+            fs.writeFileSync('build/contracts/RES.json', JSON.stringify(RESabi, null, 4))
             console.log("Newly migrated ABI: ", RES.address)
         });
-    });
 };
+
