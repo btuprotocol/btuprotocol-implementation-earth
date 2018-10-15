f785dc503beac18f4411e4f37fcb13e27b322ee3
Gad Azeraf <gad.azeraf@epita.fr>
2018-10-15 13:25:18 +0200

    Truffle migrate now in ropsten

--- demo-mvp/truffle.js
+++ demo-mvp/truffle.js
@@ -9,6 +9,15 @@ module.exports = {
       port: 9545,           // truffle develop port
       network_id: "*",      // Match any network id
       gas: 4712388          // Standard gas limit
+    },
+    ropsten: {
+        provider: function() {
+            return new HDWalletProvider(mnemonic,
+"https://ropsten.infura.io/DYBja4A1RKCdnSP4DMYt");
+        },
+        port: 443,
+        network_id: "3",
+        gas: 4712388          // Standard gas limit
     }
   }
 };
